f7b89755020d5429a5571a09c7c714a6b27889f5
joesid <joesidious@gmail.com>
2023-08-20 22:55:34 +0100

    2nd Attempt - Make file extension recognizable for PIL, Copy after changing format

--- Scale and convert images using PIL/script.py
+++ Scale and convert images using PIL/script.py
@@ -4,8 +4,6 @@ import os
 from PIL import Image
 import shutil
 
-
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
 #print("script directory: ", script_dir)
 
@@ -22,18 +20,26 @@ sav_path = script_dir + "/opt/icons/"
 #Change directory permission
 os.chmod(sav_path, 0o777)
 
-for filename in os.listdir(dir):
-   file_path = os.path.join(dir, filename)
+file_list = os.listdir(dir)
 
-   image = Image.open(file_path)
+for old_filename in file_list:
+   if os.path.isfile(os.path.join(dir, old_filename)):
+      new_filename = old_filename + ".tiff"
+      os.rename(os.path.join(dir, old_filename),os.path.join(dir, new_filename))
 
-   rotated_image = image.rotate(90)
+      file_list2 = os.listdir(dir)
 
-   resized_image = rotated_image.resize((128, 128))
 
-   rgb_image = resized_image.convert("RGB")
 
-   rgb_image.save(sav_path, "JPEG")
+      sav_path = script_dir + "/opt/icons/"
 
-# for filename in os.listdir(dir):
-#    shutil.copy2(dir, sav_path)
+      for filename in os.listdir(sav_path):
+
+           file_path = os.path.join(sav_path, filename)
+
+           image = Image.open(file_path)
+
+      for file_name in file_list2:
+         source_file = os.path.join(dir, file_name)
+         destination_file = os.path.join(sav_path, file_name)
+         shutil.copy(source_file, destination_file)
