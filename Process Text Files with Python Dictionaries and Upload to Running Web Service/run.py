05d9202c7b0413cab2fd015899d5ef19aeae3e49
joesid <joesidious@gmail.com>
2023-09-04 21:58:15 +0100

    Send POST Request

--- Process Text Files with Python Dictionaries and Upload to Running Web Service/run.py
+++ Process Text Files with Python Dictionaries and Upload to Running Web Service/run.py
@@ -21,5 +21,17 @@ def readlines(files):
         lines = []
     return lines
 
+feedback = []
+keys = ['title', 'name', 'date', 'feedback']
+for file in files:
+    lines = readlines(file)
+    feedback.append(dict(zip(keys, lines)))
 
+url = "http://localhost/feedback/"
 
+for entry in feedback:
+    response = requests.post(url, data=entry)
+    if response.ok:
+        print("loaded entry")
+    else:
+        print(f"load entry error: {response.status_code}")
